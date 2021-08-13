efcdb526cebe641f07e4aad36e75b43dde1d82b2
Siddharth Dhaigude <sd8capricon@gmail.com>
2021-08-13 12:18:41 +0530

    db ssl reject unAuthorized

--- db/index.js
+++ db/index.js
@@ -1,7 +1,10 @@
 const {Pool} = require('pg');
 const connectionString = process.env.NODE_ENV === 'production' ? process.env.DATABASE_URL : process.env.PG_URI;
 
-const pool = new Pool({connectionString});
+const pool = new Pool({
+    connectionString,
+    ssl: { rejectUnauthorized: false }
+});
 
 module.exports = {
     query: (text, params) => pool.query(text, params),
